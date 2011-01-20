24f6507ba1ac5ffbaf170eac6c7cc7a2ae7b3e66
Iwein Fuld <iwein@fuld.net>
2011-01-20 13:55:20 +0100

    Add test for locking multiplexer, still green because it's in a single thread.

--- src/main/java/com/example/LockingLineMultiplexer.java
+++ src/main/java/com/example/LockingLineMultiplexer.java
@@ -1,24 +1,32 @@
 package com.example;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 
 /**
  * @author iwein
  */
-public class LockingLineMultiplexer implements LineMultiplexer{
+public class LockingLineMultiplexer implements LineMultiplexer {
 
     private final Map<String, Queue<String>> queueMap = new HashMap<String, Queue<String>>();
 
 
     @Override
     public void addLineForKey(String line, String key) {
-        queueMap.get(key).offer(line);
+        getQueueForKey(key).offer(line);
+    }
+
+    private Queue<String> getQueueForKey(String key) {
+        if(!queueMap.containsKey(key)){
+            queueMap.put(key, new LinkedList<String>());
+        }
+        return queueMap.get(key);
     }
 
     @Override
     public String nextLineFor(String key) {
-        queueMap.get(key).poll();
+        return getQueueForKey(key).poll();
     }
 }
---
+++ src/test/java/com/example/LockingLineMultiplexerTest.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author iwein
+ */
+public class LockingLineMultiplexerTest {
+
+    private final LockingLineMultiplexer multiplexer = new LockingLineMultiplexer();
+
+    @Test
+    public void shouldReturnLineAddedForKey() throws Exception {
+        multiplexer.addLineForKey("This is the line", "Key");
+        assertThat(multiplexer.nextLineFor("Key"), is("This is the line"));
+    }
+
+    @Test
+    public void shouldReturnLinesInRightOrder() throws Exception {
+        String line1 = "This is the first line";
+        String key1 = "Key";
+        String line2 = "This is the first line";
+        String key2 = "Key";
+        multiplexer.addLineForKey(line1, key1);
+        multiplexer.addLineForKey(line2, key2);
+        assertThat(multiplexer.nextLineFor(key1), is(line1));
+        assertThat(multiplexer.nextLineFor(key2), is(line2));
+    }
+}
