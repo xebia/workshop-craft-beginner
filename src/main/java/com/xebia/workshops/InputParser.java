5d36f49baa2de1dc67b0a22a61bd1412f5e0c38a
Age Mooij <age.mooij@gmail.com>
2011-01-21 13:30:45 +0100

    Old stuff that I forgot to commit yesterday. A first implementation fo the InputParser
    Signed-off-by: Age Mooij <age.mooij@gmail.com>

--- src/main/java/com/xebia/workshops/InputParser.java
+++ src/main/java/com/xebia/workshops/InputParser.java
@@ -1,5 +1,84 @@
 package com.xebia.workshops;
 
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputParser {
+
+    public List<String> parseTitles(File input) {
+        List<String> titles = new ArrayList<String>();
+        String fileContents = null;
+
+        try {
+            fileContents = FileUtils.readFileToString(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (fileContents != null) {
+            while (true) {
+                int nextTitlePos = fileContents.indexOf("\"title\"");
+
+                if (nextTitlePos < 0) {
+                    break;
+                }
+
+                fileContents = fileContents.substring(nextTitlePos + 7);
+
+                Pattern regex = Pattern.compile(":\"([^\"]*)\"(.*)", Pattern.DOTALL);
+
+                Matcher matcher = regex.matcher(fileContents);
+
+                if (matcher.matches()) {
+                    titles.add(matcher.group(1));
+
+                    fileContents = matcher.group(2);
+                }
+            }
+        }
+
+        return titles;
+    }
+
+    public List<Long> parseIds(File input) {
+        List<Long> ids = new ArrayList<Long>();
+        String fileContents = null;
+
+        try {
+            fileContents = FileUtils.readFileToString(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (fileContents != null) {
+            while (true) {
+                int nextTitlePos = fileContents.indexOf("\"id\"");
+
+                if (nextTitlePos < 0) {
+                    break;
+                }
+
+                fileContents = fileContents.substring(nextTitlePos + 4);
+
+                Pattern regex = Pattern.compile(":(\\d*)(.*)", Pattern.DOTALL);
+
+                Matcher matcher = regex.matcher(fileContents);
+
+                if (matcher.matches()) {
+                    ids.add(Long.parseLong(matcher.group(1)));
+
+                    fileContents = matcher.group(2);
+                }
+            }
+        }
+
+        return ids;
+    }
 }
